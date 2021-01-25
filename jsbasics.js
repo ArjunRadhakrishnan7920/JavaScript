fbb09e714f4e76545c24729a30068792dc9c8ba1
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-01-25 08:15:34 +0530

    jsbasics

---
+++ jsbasics.js
@@ -0,0 +1,107 @@
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i=10;
+// while(i>=0){
+//     console.log(i);
+//     i--;
+// }
+
+
+// let i=5;
+// for(i=5;i<=15;i++){
+//     console.log(i);
+// }
+
+
+// let i;
+// for(i=15;i>=10;i--){
+//     console.log(i);
+// }
+
+// let i=2;
+// while(i<=10){
+    
+//     console.log(i);
+//     i=i+2;
+// }
+
+
+// for(let i=1;i<=10;i=i+2){
+//     console.log(i);
+// }
+
+
+// let i=4;
+// while(i<=16){
+//     console.log(i);
+//     i=i+4;
+// }
+
+// let i=6;
+// while(i<=60){
+//     console.log(i);
+//     i=i+6;
+// }
+
+
+// let i=1;
+// while(i<=24){
+//     if(24%i==0){
+//     console.log(i);
+
+//     }
+//     i++;
+
+// }
+
+
+// var a =123456,b, sum = 0;
+// while(a > 0)
+// {
+//   b = a % 10;
+//   sum = sum * 10 + b;
+//   a = parseInt(a / 10);
+// }
+// console.log(sum)
+
+
+
+// var arr = [6, 3, 34, 7, 5, 16];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 === 0) {
+//             console.log(arr[i]);
+//         }
+//     }
+
+
+
+// let n1 = 0, n2 = 1, nextTerm,number=6;
+// for (let i = 1; i <= number; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+
+
+// let sum=0,temp=153;
+// let number=temp;
+// while(temp>0){
+//     let rem=temp%10;
+//     sum+=rem*rem*rem;
+//     temp=parseInt(temp/10);
+// }
+// if(sum==number){
+//     console.log("armstrong");
+
+// }
+// else{
+//     console.log("not armstrong");
+// }
+
+
