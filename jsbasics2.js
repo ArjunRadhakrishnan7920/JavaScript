7f8299283c21e203311036960493f26bd7cf239b
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-01-25 09:01:59 +0530

    jsbasics2

---
+++ jsbasics2.js
@@ -0,0 +1,191 @@
+// let i=48;
+// if(i%7==0){
+//     console.log("divs by 7");
+// }
+// else{
+//     console.log("not divs");
+// }
+
+// let num=20;
+// if(num%3==0){
+//     console.log("multiple of three")
+// }
+// else{
+//     console.log("not multiple");
+// }
+
+
+
+// let num=-20;
+// if(num>0){
+//     console.log("possitive");
+// }
+// else{
+//     console.log("negative");
+// }
+
+
+
+
+// let num=44;
+// if(num%10==4){
+//     console.log("unit place is 4");
+// }
+// else{
+//     console.log("not 4");
+// }
+
+
+// let num=999;
+// if(num>99 && num<=999){
+//     console.log("three digit");
+// }else{
+//     console.log("not three");
+// }
+
+
+// let num=60;
+// if(num%2==0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+
+// let num1=23,num2=30;
+// if(num1>num2){
+//     console.log("num1 is bigger");
+// }
+// else{
+//     console.log("num2 is bigger");
+// }
+
+
+
+// let num1=10,num2=19;
+// if(num1>num2){
+//     console.log("num2 is smaller",num2)
+// }
+// else{
+//     console.log("num1 is smaller",num1);
+// }
+
+
+
+// let num1=43,num2=34,num3=12;
+// if(num1>num2 && num1>num3){
+//     console.log("num1 is bigger");
+
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log("num2 is bigger");
+// }else{
+//     console.log("num3 is bigger");
+// }
+
+
+
+
+// let num1=4,num2=34,num3=12;
+// if(num1<num2 && num1<num3){
+//     console.log("num1 is smaller");
+
+// }
+// else if(num2<num1 && num2<num3){
+//     console.log("num2 is smalller");
+// }else{
+//     console.log("num3 is smaller");
+// }
+
+
+
+
+
+// var x= 10;
+// var y=5;
+// var z= 2;
+// if (x>y && x>z)
+// {
+//         if (y>z)
+//         {
+//             console.log(x + ", " + y + ", " +z);
+//         }
+//         else
+//         {
+//             console.log(x + ", " + z + ", " +y);
+//         }
+// }
+// else if (y>x && y >z)
+// {
+//         if (x>z)
+//         {
+//              console.log(y + ", " + x + ", " +z);
+//         }
+//         else
+//         {
+//              console.log(y + ", " + z + ", " +x);
+//         }
+// }
+// else if (z>x && z>y)
+// {
+//     if (x>y)
+//     {
+//         console.log(z + ", " + x + ", " +y);
+//     }
+//     else
+//     {
+//         console.log(z + ", " + y + ", " +x);
+//     }
+// }        
+
+
+
+
+
+
+// var x= 10;
+// var y=5;
+// var z= 2;
+// if (x<y && x<z)
+// {
+//         if (y<z)
+//         {
+//             console.log(x + ", " + y + ", " +z);
+//         }
+//         else
+//         {
+//             console.log(x + ", " + z + ", " +y);
+//         }
+// }
+// else if (y<x && y <z)
+// {
+//         if (x<z)
+//         {
+//              console.log(y + ", " + x + ", " +z);
+//         }
+//         else
+//         {
+//              console.log(y + ", " + z + ", " +x);
+//         }
+// }
+// else if (z<x && z<y)
+// {
+//     if (x<y)
+//     {
+//         console.log(z + ", " + x + ", " +y);
+//     }
+//     else
+//     {
+//         console.log(z + ", " + y + ", " +x);
+//     }
+// }        
+
+
+
+
+
+
+
+
+
+
