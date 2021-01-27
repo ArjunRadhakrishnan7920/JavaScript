3203a0a49f4265dbf8d4abf2056589f9d697f52a
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-01-27 08:48:49 +0530

    array

---
+++ array.js
@@ -0,0 +1,112 @@
+// let colors=['red','orange','yellow','blue']
+// console.log(colors)
+
+
+
+//  let colors=['red','orange','blue']
+//  console.log(colors[0]);
+// console.log(colors[colors.length-1]);
+
+
+// let colors=['red','orange','blue']
+// for(let i=0;i<colors.length;i++){
+//     console.log(colors[i]);
+// }
+
+
+//..................................... Array of objects..........................................................................
+
+// let cars = 
+    // {
+//       "color": "purple",
+//       "type": "minivan",
+//       "registration": new Date('2017-01-03'),
+//       "capacity": 7
+//     }
+    
+// console.log(cars.color);
+
+
+//.....................................Array of objects and accessing them.....................................................
+
+
+// var donuts = [
+//     { type: "Jelly", cost: 1.22 },
+//     { type: "Chocolate", cost: 2.45 },
+//     { type: "Cider", cost: 1.59 },
+//     { type: "Boston Cream", cost: 5.99 }
+// ];
+
+// var i = 0;
+// donuts.forEach(function() {
+
+// var donut = donuts[i].type;
+// var cost = donuts[i].cost;
+
+// console.log(donut + " donuts cost $" + cost + " each");
+//     i = i + 1;
+// })
+
+
+
+// let activities = [
+//     ['Work', 9],
+//     ['Eat', 1],
+//     ['Commute', 2],
+//     ['Play Game', 1],
+//     ['Sleep', 7]
+// ];
+// console.log(activities[3][0]);
+
+
+
+// var array =[ 
+//     [12, 90],
+//     [62, 50],
+     
+// ];
+// var sum = 0;
+// //console.log(array.length);
+// for (var i = 0; i < array.length; i++) {
+//     //console.log(array[i]);
+//     for (var j = 0; j < array[i].length; j++) {
+        
+//         sum += array[i][j];
+//     }  
+
+// }
+// console.log(array[0][0]);
+// console.log(sum);
+        
+    
+//..............................Array Push.........................................
+
+
+// let colors=['red','blue','yellow']
+// colors.push('green');
+// console.log(colors);
+
+
+//.......................Array Pop.......................
+
+// let colors=['red','blue','orange'];
+//colors.pop();
+//console.log(colors);
+
+//colors.shift();
+//console.log(colors);
+
+// colors.unshift('black');
+// console.log(colors);
+
+
+
+
+
+
+
+
+
+
+
+
