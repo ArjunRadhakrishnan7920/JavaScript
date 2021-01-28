12243c374f2533a725868f278995447369b21be2
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-01-28 09:21:59 +0530

    fatarrow

---
+++ fat.js
@@ -0,0 +1,15 @@
+// var x=(a,b)=>console.log(a+10);
+// x(3);
+
+
+// var x=(a,b)=>console.log(a+10);
+// x(3);
+
+// hello = () =>console.log( this);
+// hello();
+
+
+// function hello(){
+//     console.log(this);
+// }
+// hello();
