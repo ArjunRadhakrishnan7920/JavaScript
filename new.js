7e75832653516e11d2064ace504e39119ab63442
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-01-29 07:50:13 +0530

    foreach

--- new.js
+++ new.js
@@ -1,1 +1,40 @@
-dcddc
+// let emp={
+//     name:'Arjun',
+//     email:'arjun@gmail.com',
+//     mobile:96773323,
+//     info:()=>{
+//         console.log('hello');
+//     }
+// }
+// console.log(emp.sal=20);
+// console.log(emp['mobile']);
+
+
+// delete emp.mobile;
+
+// console.log(emp);
+
+
+//----------------------------------------------for each &&& map-------------------------------------------------------------
+
+// let size=[10,12,14]
+// size.forEach(()=>{});
+// console.log(size);
+
+
+// let size=[10,12,14];
+// size.forEach((x)=>{
+//     console.log(x+2);
+// });
+
+
+// size.forEach((x)=>console.log(x+2));
+
+// let size=[12,13,14];
+// let newValue=size.map((x)=>x+2);
+// console.log(newValue);
+
+
+// let size=[12,13,14];
+// let newValue=size.forEach((x)=>x+2);
+// console.log(newValue);
