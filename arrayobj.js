f627305fb114ce2348db127d88b0e61acef34708
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-02-01 19:35:30 +0530

    arrayobj

---
+++ arrayobj.js
@@ -0,0 +1,27 @@
+let products=[
+    {name:'Apple',price:3000,color:'red'},
+    {name:'ore',price:2000,color:'blu'},
+    {name:'sle',price:1000,color:'neon'},
+    {name:'le',price:13000,color:'iod'}
+]
+
+// for(x of products){
+//     console.log(x);
+// }
+
+
+
+// let newpro=products.filter((x)=>{
+//     if(x.price>=2000)  
+//      return x;
+// }
+// )
+// console.log(newpro);
+  
+
+// let x=products.filter((product)=>{
+//     if(product.color==='red'|| product.color==='neon')
+//     return product;
+
+// })
+// console.log(x);
