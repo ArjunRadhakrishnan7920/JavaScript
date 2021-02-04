31104db989cc95ebd35accdb6611e06268b8141f
ArjunRadhakrishnan7920 <arjunrillathu@gmail.com>
2021-02-04 09:08:35 +0530

    spread

---
+++ spread/spread.js
@@ -0,0 +1,20 @@
+// let emp={
+//     name:"awss",
+//     email:"aeek",
+// }
+
+// let doj={
+//     date:"sept",
+//     email:"mcsk",
+// }
+
+// let x={
+//    obj1={ ...emp},
+//     obj2={...doj},
+// }
+// console.log(x);
+
+
+
+let array=[2,8,9,4,12,17];
+console.log(array.sort());
